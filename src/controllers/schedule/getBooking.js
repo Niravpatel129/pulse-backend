691ec2cd5c2bffa04eaa4c2b6219a71531021114
Nirav <niravpatelp129@gmail.com>
2025-03-28 07:14:47 -0400

    cl

--- src/controllers/schedule/getBooking.js
+++ src/controllers/schedule/getBooking.js
@@ -1,4 +1,6 @@
+import Availability from '../../models/Availability.js';
 import BookingRequest from '../../models/BookingRequest.js';
+import GoogleCalendar from '../../models/GoogleCalendar.js';
 import AppError from '../../utils/AppError.js';
 
 /**
@@ -17,10 +19,18 @@ const getBooking = async (req, res, next) => {
       return next(new AppError('Booking not found', 404));
     }
 
+    // Get user's Google Calendar and Availability information
+    const [googleCalendar, availability] = await Promise.all([
+      GoogleCalendar.findOne({ user: booking.userId }),
+      Availability.findOne({ userId: booking.userId }),
+    ]);
+
     res.status(200).json({
       status: 'success',
       data: {
         booking,
+        googleCalendar,
+        availability,
       },
     });
   } catch (error) {
