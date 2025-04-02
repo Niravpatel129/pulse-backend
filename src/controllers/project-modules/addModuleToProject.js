dc05f2632a6c1f30933b99cee7b12279af24bf3a
Nirav <niravpatelp129@gmail.com>
2025-04-02 04:03:00 -0400

    cleanup

--- src/controllers/project-modules/addModuleToProject.js
+++ src/controllers/project-modules/addModuleToProject.js
@@ -2,29 +2,33 @@ import File from '../../models/fileModel.js';
 import ProjectModule from '../../models/ProjectModule.js';
 
 const addModuleToProject = async (req, res) => {
-  const { projectId, moduleType, moduleContent } = req.body;
+  try {
+    const { projectId, moduleType, moduleContent } = req.body;
 
-  if (moduleType === 'file') {
-    const fileId = moduleContent.fileId;
+    if (moduleType === 'file') {
+      const fileId = moduleContent.fileId;
 
-    const file = await File.findById(fileId);
+      const file = await File.findById(fileId);
 
-    if (!file) {
-      return res.status(404).json({ message: 'File not found' });
+      if (!file) {
+        return res.status(404).json({ message: 'File not found' });
+      }
+
+      const projectModule = await ProjectModule.create({
+        project: projectId,
+        addedBy: req.user.userId,
+        moduleType: 'file',
+        name: moduleContent.fileName,
+        content: {
+          fileId: fileId,
+        },
+      });
+      return res.status(201).json(projectModule);
+    } else {
+      return res.status(400).json({ message: 'Invalid module type' });
     }
-
-    const projectModule = await ProjectModule.create({
-      project: projectId,
-      addedBy: req.user.userId,
-      moduleType: 'file',
-      name: moduleContent.fileName,
-      content: {
-        fileId: fileId,
-      },
-    });
-    return res.status(201).json(projectModule);
-  } else {
-    return res.status(400).json({ message: 'Invalid module type' });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
   }
 };
 
