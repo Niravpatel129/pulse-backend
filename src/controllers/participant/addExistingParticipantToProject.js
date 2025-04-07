dad8341a9a3152cad4bf0e402f24656db52cde2f
Nirav <niravpatelp129@gmail.com>
2025-04-07 17:28:32 -0400

    2

--- src/controllers/participant/addExistingParticipantToProject.js
+++ src/controllers/participant/addExistingParticipantToProject.js
@@ -1,4 +1,5 @@
 import Participant from '../../models/Participant.js';
+import Project from '../../models/Project.js';
 import ApiError from '../../utils/apiError.js';
 import ApiResponse from '../../utils/apiResponse.js';
 
@@ -16,15 +17,41 @@ export const addExistingParticipantToProject = async (req, res, next) => {
 
     const workspaceId = req.workspace._id;
 
-    const participant = await Participant.findOne({
-      _id: participantId,
-      workspace: workspaceId,
-    });
+    // Find both the participant and project
+    const [participant, project] = await Promise.all([
+      Participant.findOne({
+        _id: participantId,
+        workspace: workspaceId,
+      }),
+      Project.findById(projectId),
+    ]);
 
     if (!participant) {
       throw new ApiError(404, 'Participant not found');
     }
 
+    if (!project) {
+      throw new ApiError(404, 'Project not found');
+    }
+
+    // Check if participant is already in the project
+    const alreadyInProject = project.participants.find(
+      (p) => p.participant.toString() === participantId.toString(),
+    );
+
+    if (alreadyInProject) {
+      return res
+        .status(200)
+        .json(new ApiResponse(200, participant, 'Participant is already in this project'));
+    }
+
+    // Add participant to project's participants array
+    project.participants.push({
+      participant: participantId,
+    });
+    await project.save();
+
+    // Update participant's project reference
     participant.project = projectId;
     await participant.save();
 
