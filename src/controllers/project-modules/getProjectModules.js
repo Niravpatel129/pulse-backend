ab12b8b58bbdea12dfa890688c0c718fb541b225
Nirav <niravpatelp129@gmail.com>
2025-04-15 16:13:48 -0400

    2

--- src/controllers/project-modules/getProjectModules.js
+++ src/controllers/project-modules/getProjectModules.js
@@ -1,14 +1,19 @@
 import ProjectModule from '../../models/ProjectModule.js';
 
 const getProjectModules = async (req, res) => {
-  const { projectId } = req.params;
-  const projectModules = await ProjectModule.find({ project: projectId })
-    .populate('content.fileId')
-    .populate('content.templateId')
-    .populate('addedBy')
-    .populate('versions.updatedBy')
-    .populate('versions.contentSnapshot.fileId');
-  res.status(200).json(projectModules);
+  try {
+    console.log('getProjectModules');
+    const { projectId } = req.params;
+    const projectModules = await ProjectModule.find({ project: projectId })
+      .populate('content.fileId')
+      .populate('content.templateId')
+      .populate('addedBy')
+      .populate('versions.updatedBy')
+      .populate('versions.contentSnapshot.fileId');
+    res.status(200).json(projectModules);
+  } catch (error) {
+    next(error);
+  }
 };
 
 export default getProjectModules;
