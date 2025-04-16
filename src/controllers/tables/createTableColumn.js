b11f603b7bcad6c26afd40eadf133e7f43927f7d
Nirav <niravpatelp129@gmail.com>
2025-04-16 18:50:09 -0400

    2

---
+++ src/controllers/tables/createTableColumn.js
@@ -0,0 +1,74 @@
+import mongoose from 'mongoose';
+import Table from '../../models/Table/Table.js';
+import AppError from '../../utils/AppError.js';
+
+/**
+ * Create a new column in a table
+ * @param {Object} req - Express request object
+ * @param {Object} res - Express response object
+ * @param {Function} next - Express next middleware function
+ */
+const createTableColumn = async (req, res, next) => {
+  try {
+    const { tableId } = req.params;
+    const columnData = req.body;
+    const workspaceId = req.workspace._id;
+
+    // Find the table and ensure it belongs to the current workspace
+    const table = await Table.findOne({
+      _id: tableId,
+      workspace: workspaceId,
+    });
+
+    if (!table) {
+      return next(new AppError('Table not found or you do not have access to this table', 404));
+    }
+
+    // get the last order of the columns
+    const lastColumn = table.columns[table.columns.length - 1];
+    const newOrder = lastColumn ? lastColumn.order + 1 : 0;
+
+    // Transform the incoming column data to match our schema
+    const newColumn = {
+      id: new mongoose.Types.ObjectId().toString(),
+      name: columnData.name,
+      type: columnData.type, // Extract the type ID from the type object
+      icon: columnData.icon || 'text',
+      options: columnData.options,
+      order: newOrder,
+    };
+
+    // Add additional options based on column type
+    if (['single_select', 'multi_select'].includes(columnData.type.id)) {
+      newColumn.options.selectOptions = [];
+    } else if (columnData.type.id === 'currency') {
+      newColumn.options.currencySymbol = '$';
+      newColumn.options.hasDecimals = true;
+      newColumn.options.decimalPlaces = 2;
+    } else if (columnData.type.id === 'number') {
+      newColumn.options.hasDecimals = false;
+    } else if (columnData.type.id === 'date') {
+      newColumn.options.dateFormat = 'MM/DD/YYYY';
+    } else if (columnData.type.id === 'phone') {
+      newColumn.options.phoneFormat = 'international';
+    } else if (columnData.type.id === 'checkbox') {
+      newColumn.options.defaultChecked = false;
+    } else if (columnData.type.id === 'rating') {
+      newColumn.options.maxValue = '5';
+    }
+
+    // Add the new column to the table
+    table.columns.push(newColumn);
+    await table.save();
+
+    res.status(200).json({
+      success: true,
+      data: table,
+      message: 'New column added successfully',
+    });
+  } catch (error) {
+    next(error);
+  }
+};
+
+export default createTableColumn;
