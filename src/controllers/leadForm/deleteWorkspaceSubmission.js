ec0fcd536ed06bf2f9bcb5e24a75df0dfdd7c525
Nirav <niravpatelp129@gmail.com>
2025-04-20 19:28:16 -0400

    2

--- src/controllers/leadForm/deleteWorkspaceSubmission.js
+++ src/controllers/leadForm/deleteWorkspaceSubmission.js
@@ -11,12 +11,19 @@ const deleteWorkspaceSubmission = async (req, res) => {
       return res.status(404).json({ message: 'Submission not found' });
     }
 
+    // Find the submission before deleting it
+    const submission = leadForm.submissions.find((submission) => submission._id.toString() === id);
+
+    // Filter out the submission to delete it
     leadForm.submissions = leadForm.submissions.filter(
       (submission) => submission._id.toString() !== id,
     );
     await leadForm.save();
 
-    res.json({ message: 'Submission deleted successfully' });
+    res.json({
+      message: 'Submission deleted successfully',
+      formTitle: leadForm.title,
+    });
   } catch (error) {
     handleError(res, error);
   }
