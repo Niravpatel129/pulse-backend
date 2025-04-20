a571e48922c9bb9c49ed53a9e18a466a9058462d
Nirav <niravpatelp129@gmail.com>
2025-04-20 19:49:07 -0400

    2

--- src/controllers/leadForm/getWorkspaceSubmissions.js
+++ src/controllers/leadForm/getWorkspaceSubmissions.js
@@ -4,12 +4,29 @@ import { handleError } from '../../utils/errorHandler.js';
 const getWorkspaceSubmissions = async (req, res) => {
   try {
     const workspace = req.workspace;
-    const submissions = await LeadForm.find({ workspace: workspace._id })
-      .select('submissions')
+    const forms = await LeadForm.find({ workspace: workspace._id })
+      .select('title submissions')
       .lean()
       .sort({ createdAt: -1 });
 
-    res.json(submissions);
+    // Inject formTitle into each submission
+    const formsWithTitleInSubmissions = forms.map((form) => {
+      const { title, submissions = [], ...rest } = form;
+
+      // Add the formTitle to each submission
+      const updatedSubmissions = submissions.map((submission) => ({
+        ...submission,
+        formTitle: title,
+      }));
+
+      return {
+        ...rest,
+        title,
+        submissions: updatedSubmissions,
+      };
+    });
+
+    res.json(formsWithTitleInSubmissions);
   } catch (error) {
     handleError(res, error);
   }
