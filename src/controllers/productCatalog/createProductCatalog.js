d5b167fa853b2e8d7dac7895a9474edddffa64d9
Nirav <niravpatelp129@gmail.com>
2025-04-22 19:24:24 -0400

    2

--- src/controllers/productCatalog/createProductCatalog.js
+++ src/controllers/productCatalog/createProductCatalog.js
@@ -3,7 +3,7 @@ import AppError from '../../utils/AppError.js';
 
 export const createProductCatalog = async (req, res, next) => {
   try {
-    const { name, quantity, price, projects, modules } = req.body;
+    const { name, quantity, price, projects, modules, discount } = req.body;
 
     const product = await ProductCatalog.create({
       name,
