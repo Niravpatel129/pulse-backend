78c63711989a1b614f88909092cfc6b9d4c68702
Nirav <niravpatelp129@gmail.com>
2025-04-24 17:23:55 -0400

    2

--- src/config/middleware/validateRequest.js
+++ src/config/middleware/validateRequest.js
@@ -1,11 +1,5 @@
 const validateRequest = (schema) => {
   return (req, res, next) => {
-    console.log('Raw request body:', req.body);
-    console.log('Content-Type:', req.headers['content-type']);
-    console.log('Request method:', req.method);
-    console.log('Request path:', req.path);
-    console.log('Request headers:', req.headers);
-
     // Create a copy of the request body for validation
     const validationBody = { ...req.body };
     console.log('Validation body (initial):', validationBody);
