9d9e9753c743a990378932a4b7a88d8a7bab86d6
Nirav <niravpatelp129@gmail.com>
2025-05-19 02:24:24 -0400

    2

--- src/controllers/integrations/gmail/getAuthUrl.js
+++ src/controllers/integrations/gmail/getAuthUrl.js
@@ -37,7 +37,7 @@ const getAuthUrl = asyncHandler(async (req, res) => {
       prompt: 'consent', // Force consent screen to get refresh token
       include_granted_scopes: true, // Include any previously granted scopes
       response_type: 'code',
-      state: state, // Include workspace info in state
+      state: encodeURIComponent(state), // Properly encode the state parameter
     });
 
     res.status(200).json({
