dc98b1c179dacf558ff750546d7c1dae5bcbec91
Nirav <niravpatelp129@gmail.com>
2025-05-28 00:30:45 -0400

    2

--- src/utils/aiUtils.js
+++ src/utils/aiUtils.js
@@ -12,7 +12,7 @@ export const summarizeMessages = async (messages) => {
     }
 
     const response = await openai.chat.completions.create({
-      model: 'gpt-4',
+      model: 'gpt-3.5-turbo',
       messages: [
         {
           role: 'system',
