e7ec92f004a289f7871820eeaec2f8c776257818
Nirav <niravpatelp129@gmail.com>
2025-06-21 13:42:21 -0400

    2

--- src/routes/publicRoutes.js
+++ src/routes/publicRoutes.js
@@ -1,10 +1,16 @@
 import express from 'express';
+import rateLimit from 'express-rate-limit';
 import inboundEmailController, {
   handleFileUploads,
 } from '../controllers/public/inboundEmailController.js';
 
 const router = express.Router();
 
-router.post('/inbound', handleFileUploads, inboundEmailController);
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 100, // limit each IP to 100 requests per windowMs
+});
+
+router.post('/inbound', limiter, handleFileUploads, inboundEmailController);
 
 export default router;
